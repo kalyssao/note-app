e52e6e3beee39a9e8f95d0abb8bcdc5b6cf862e4
kalyssao <klysssa@icloud.com>
2020-12-07 12:33:29 +0000

    styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { Route } from 'react-router-dom'
-import Header from './components/Header'
 import Landing from './views/Landing'
 import Login from './components/Login'
 import Home from './views/Home'
@@ -8,16 +7,16 @@ import Profile from './views/Profile'
 
 const appDiv = {
     'textAlign': 'center',
-    'width': '75%',
+    'width': '50%',
     'margin': '0 auto',
-    'paddingTop': '20%'
+    'padding': '15%',
+
 }
 
 function App() {
     // const [isLoggedIn, setIsLoggedIn] = useState(false)
   return (
     <div style={appDiv}>
-        <Header/>
         <Route exact path="/"><Landing/></Route>
         <Route path="/login"><Login/></Route>
         <Route path="/home"><Home/></Route>
