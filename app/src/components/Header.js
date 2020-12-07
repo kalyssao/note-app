dd680db20b7179f45d9932cdfe1cf369b0ca9d5e
kalyssao <klysssa@icloud.com>
2020-12-07 12:18:41 +0000

    made header contain logout

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { React, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Button } from '@geist-ui/react'
 
 export default function Header() {
+    let history = useHistory()
+    const [loggedIn, isLoggedIn] = useState(false)
+    
+    function logOut(){
+        localStorage.setItem('loggedIn', 'false')
+        history.push('/')
+    }
     return (
         <nav>
             <h3>notes.</h3>
+            {
+                localStorage.getItem('loggedIn') === "true"
+                ? <Button onClick={logOut}>Log Out</Button>
+                : <></>
+            }
         </nav>
     )
 }
