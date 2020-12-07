c93aaad9ed762152ab70bc63c9f8a5654c6d310e
kalyssao <klysssa@icloud.com>
2020-12-07 12:28:53 +0000

    added header

--- app/src/views/Landing.js
+++ app/src/views/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { Button } from '@geist-ui/react'
+import Header from '../components/Header'
 
 export default function Landing() {
     let history = useHistory()
@@ -9,6 +10,7 @@ export default function Landing() {
     }
     return (
         <div>
+            <Header/>
             <Button onClick={logIn}>Log In</Button>
         </div>
     )
